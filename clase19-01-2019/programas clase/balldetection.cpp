fcce71019ed67d8d40ec2c48d554a319fc7e588a
gfdgeras <gfdgeras@Gerardos-MacBook-Air-2.local>
2019-01-23 18:18:07 -0600

    clase 19012019 programa cpp

--- clase19-01-2019/programas clase/balldetection.cpp
+++ clase19-01-2019/programas clase/balldetection.cpp
@@ -2,13 +2,14 @@
 //g++ $(pkg-config --cflags --libs opencv) -std=c++11  programaClase.cpp -o yourFileProgram
 #include "opencv2/opencv.hpp"
 using namespace cv;
+using namespace std;
 int main(int, char**)
 {
     int circulosMax = 5;
     circulosMax = circulosMax -1;
     vector<Vec3f> circles;
-    Scalar min = cv::Scalar(0,0,66); 
-    Scalar max = cv::Scalar(162,114,255);    
+    Scalar min = Scalar(0,0,66); 
+    Scalar max = Scalar(162,114,255);    
     Mat frame;
     Mat hsv;    
     Mat mask;
@@ -24,18 +25,19 @@ int main(int, char**)
         
         cap >> frame; 
         cvtColor(frame, hsv, COLOR_BGR2HSV);
-        Scalar min = cv::Scalar(98,99,87); 
-        Scalar max = cv::Scalar(115,255,255);
+        Scalar min = Scalar(0,0,66); 
+        Scalar max = Scalar(162,114,255);
 
         inRange(hsv, min, max, mask);
-        adaptiveThreshold(mask, ,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,19,3)
+        adaptiveThreshold(mask, frame2,255,ADAPTIVE_THRESH_GAUSSIAN_C,THRESH_BINARY,19,3);
  
         erode(frame2, filtro1, getStructuringElement(MORPH_RECT, Size(3, 3)) );
         dilate(filtro1, filtro2, getStructuringElement(MORPH_RECT, Size(3, 3)) );
-
-        HoughCircles( filtro2, circles, CV_HOUGH_GRADIENT, 1, filtro2.rows/8, 200, 100, 5, 100 );
-         for( int i = 0; i <circulosMax; i++ )
-        {
+    
+        HoughCircles( filtro2, circles, HOUGH_GRADIENT, 1, 10, 100, 15, 5, 100 );
+        if (circles.size()>0){
+            for( int i = 0; i <circulosMax; i++ )
+            {
             Vec3i c = circles[i];
             Point center = Point(c[0], c[1]);
             // circle center
@@ -43,8 +45,11 @@ int main(int, char**)
             // circle outline
             int radius = c[2];
             circle( frame, center, radius, Scalar(255,0,255), 3, LINE_AA);
+            }
         }
+     
         imshow("frame", frame);
+        imshow("filtros", filtro2);
         if(waitKey(30) >= 0) break;
     }
     
